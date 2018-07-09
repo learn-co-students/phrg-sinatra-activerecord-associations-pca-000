d2fe054a2a54baa820a7e5c8a3c480a1cd1843b5
bkrigel <benjamin.krigel@gmail.com>
2018-07-09 16:36:31 -0400

    Had to fiddle around with adding [4.2] to my migrations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require_relative '../config/environment.rb'
 
+if ActiveRecord::Base.connection.migration_context.needs_migration?
+  raise Migrations are pending. Run `rake db:migrate` to resolve the issue.
+end
+
 RSpec.configure do |config|
 
 end
